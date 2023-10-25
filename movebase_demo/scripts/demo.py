1bdb775f64ad387a94f4ba331e067eca87ea1378
Devson Butani <96811105+Aeolus96@users.noreply.github.com>
2023-10-25 16:55:06 -0400

    Update demo.py

--- movebase_demo/scripts/demo.py
+++ movebase_demo/scripts/demo.py
@@ -2,6 +2,39 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
+from std_srvs.srv import Trigger
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+def movebase_client():
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = 2.7
+    goal.target_pose.pose.position.y = 0.5
+    goal.target_pose.pose.orientation.w = 1.0
+
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
+    
+
+def trigger_head_scan():
+    rospy.wait_for_service('/funmap/trigger_head_scan')
+    try:
+        trigger_scan = rospy.ServiceProxy('/funmap/trigger_head_scan', Trigger)
+        response = trigger_scan()
+        return response.message
+    except rospy.ServiceException as e:
+        rospy.logerr(f"Service call failed: {e}")
+        return None
 
 def send_goal_to_topic(x):
     print("publisher set")
@@ -34,13 +67,15 @@ if __name__ == '__main__':
     try:
         print("Starting points!")
         rospy.init_node('movebase_client_py')
-        result = send_goal_to_topic(2)
-        rospy.loginfo("1 Goal execution done!")
-        rospy.sleep(15)
-        result = send_goal_to_topic(1)
-        rospy.loginfo("2 Goal execution done!")
-        rospy.sleep(15)
-        result = send_goal_to_topic(3)
-        rospy.loginfo("Home Goal execution done!")
+        movebase_client()
+        trigger_head_scan()
+        # result = send_goal_to_topic(2)
+        # rospy.loginfo("1 Goal execution done!")
+        # rospy.sleep(15)
+        # result = send_goal_to_topic(1)
+        # rospy.loginfo("2 Goal execution done!")
+        # rospy.sleep(15)
+        # result = send_goal_to_topic(3)
+        # rospy.loginfo("Home Goal execution done!")
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
